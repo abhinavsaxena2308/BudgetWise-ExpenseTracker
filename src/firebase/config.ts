7c079e861a2f8dbcd77cf2b896002f64b22305c0
CUO ABHINAV_6565 <abhinav.saxena.2003@gmail.com>
2025-10-02 19:34:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5944973967-1d93c",
+  "appId": "1:834660803921:web:e6d6672d783345476e1f70",
+  "apiKey": "AIzaSyAZ39RbNqtRvcUVtA1hZLp2hOfr5CbRnNo",
+  "authDomain": "studio-5944973967-1d93c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "834660803921"
+};
