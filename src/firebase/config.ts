4ca84e52fb3098b0d55b87f5396cc3a2cc963366
abhinavsaxena2308 <abhinav.saxena.2003@gmail.com>
2025-10-03 12:10:04 +0530

    .env update

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-5944973967-1d93c",
-  "appId": "1:834660803921:web:e6d6672d783345476e1f70",
-  "storageBucket": "studio-5944973967-1d93c.appspot.com",
-  "apiKey": "AIzaSyAZ39RbNqtRvcUVtA1hZLp2hOfr5CbRnNo",
-  "authDomain": "studio-5944973967-1d93c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "834660803921"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
